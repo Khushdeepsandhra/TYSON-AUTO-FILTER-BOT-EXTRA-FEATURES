976de3bb5e589964d487c554970d392658f1b5a5
Tysonmovierulz <100193933+Tysonmovierulz@users.noreply.github.com>
2022-05-07 12:22:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,7 +43,7 @@ class Bot(Client):
 
     async def stop(self, *args):
         await super().stop()
-        logging.info("Restarting Ajax.")
+        logging.info("Restarting Dark.")
 
 
 app = Bot()
